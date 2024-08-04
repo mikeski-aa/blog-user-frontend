e2902f368551c6470f4e82b13c58d0d9a2e6da07
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-08-04 16:15:20 +0200

    Working on testing token thingy

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,28 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+async function test() {
+  const user = {
+    username: "1",
+    password: "1",
+  };
+  try {
+    const url = "http://localhost:3000/user/profile";
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 function handleUserInput(e, setUsername) {
   console.log(e.target.value);
   setUsername(e.target.value);
@@ -13,7 +35,7 @@ function handlePasswordInput(e, setPassword) {
   setPassword(e.target.value);
 }
 
-async function handleFormSubmit(username, password) {
+async function handleFormRegister(username, password) {
   const user = {
     username: username,
     password: password,
@@ -21,6 +43,36 @@ async function handleFormSubmit(username, password) {
 
   try {
     const url = "http://localhost:3000/user/register";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}`);
+    }
+    const json = await response.json();
+
+    // here I probably need to add something to handle if the response is an error
+    // or if a user already exists, console logs are not enough for the user
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function handleFormLogin(username, password) {
+  const user = {
+    username: username,
+    password: password,
+  };
+
+  try {
+    const url = "http://localhost:3000/user/login";
     const response = await fetch(url, {
       method: "POST",
       headers: {
@@ -63,7 +115,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <button onClick={() => userTest()}>User test</button>
+        <button onClick={() => test()}>User test</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -86,8 +138,31 @@ function App() {
             onChange={(e) => handlePasswordInput(e, setPassword)}
           />
         </div>
-        <button onClick={() => handleFormSubmit(username, password)}>
+        <button onClick={() => handleFormRegister(username, password)}>
           Register
+        </button>
+      </form>
+
+      <form method="none" onSubmit={(e) => e.preventDefault()}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter your username"
+            onChange={(e) => handleUserInput(e, setUsername)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => handlePasswordInput(e, setPassword)}
+          />
+        </div>
+        <button onClick={() => handleFormLogin(username, password)}>
+          Login
         </button>
       </form>
     </>
